7afe1b07aa1d8276622c76b32ec756aaee27ed8c
Yassin522 <yasinalmhdi8@gmail.com>
2022-08-15 16:13:02 +0300

    finally

---
+++ lib/models/config.dart
@@ -0,0 +1,12 @@
+//read the doc first
+
+
+
+
+class Config{
+  final String appName = 'Wallme';   //app name
+  
+  final String testerPassword = 'tester';   //testing password
+  //don't use this password in the database
+  //you can change this too.
+}
